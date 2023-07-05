478e3c01c0587d24df2eff4100b2e4eb9d0fc28d
polarker <polarker@users.noreply.github.com>
2023-07-05 11:53:50 +0000

    deploy: 73016540feb603674cf6f1757cf753d4f5bf4b84

---
+++ _next/static/jNHHOsH-ilff3pmGc6ERR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/509-d1a9c4e19f94a314.js","static/chunks/pages/index-1437ff9c240ec974.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
